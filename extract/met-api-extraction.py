b615403496cc234ddd2c9702e0d2aa8b5c157742
Rowan Houston <rowanhouston2@gmail.com>
2026-06-03 17:04:56 -0400

    removed old code and added gitignore

--- extract/met-api-extraction.py
+++ extract/met-api-extraction.py
@@ -1,21 +1,30 @@
 import requests
 import json
 import os
+import time
 
-extract_dir = os.path.dirname(os.path.abspath(__file__))
+project_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+raw_dir = os.path.join(project_root, "raw")
+os.makedirs(raw_dir, exist_ok=True)
 extraction_path = os.path.join(raw_dir, "met_objects.ndjson")
 
-# ids_response = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/objects")
-# ids_list = ids_response.json()["objectIDs"]
+ids_response = requests.get("https://collectionapi.metmuseum.org/public/collection/v1/objects")
+ids_list = ids_response.json()["objectIDs"]
 
-# fetched_ids = set()
+fetched_ids = set()
+if os.path.exists("raw/met_objects.ndjson"):
+    with open("raw/met_objects.ndjson", "r") as f:
+        for line in f:
+            record = json.loads(line)
+            fetched_ids.add(record["objectID"])
 
-# object_num = 1
-# url = f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{object_num}"
-# r = requests.get(url)
-# if r.status_code == 200:
-#     print("Request was successful!")
-# else:
-#     print(f"Failed to retrieve data. Status code: {r.status_code}")
-# response_json = r.json()
-# print(response_json)
+with open("raw/met_objects.ndjson", "a") as f:
+    for object_id in ids_list:
+        if object_id in fetched_ids:
+            continue
+        response = requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{object_id}")
+        if response.status_code == 200:
+            record = response.json()
+            f.write(json.dumps(record) + "\n")
+        time.sleep(0.1)
+
