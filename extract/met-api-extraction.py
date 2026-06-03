9d4bd2196629152b85be75dfe55c55050f51232a
Rowan Houston <rowanhouston2@gmail.com>
2026-06-03 18:34:53 -0400

    added user agent and checkpointing to api extraction

--- extract/met-api-extraction.py
+++ extract/met-api-extraction.py
@@ -12,19 +12,26 @@ ids_response = requests.get("https://collectionapi.metmuseum.org/public/collecti
 ids_list = ids_response.json()["objectIDs"]
 
 fetched_ids = set()
-if os.path.exists("raw/met_objects.ndjson"):
-    with open("raw/met_objects.ndjson", "r") as f:
+if os.path.exists(extraction_path):
+    with open(extraction_path, "r") as f:
         for line in f:
             record = json.loads(line)
             fetched_ids.add(record["objectID"])
 
-with open("raw/met_objects.ndjson", "a") as f:
+
+headers = {"User-Agent": "art-etl-project/1.0 (rowanhouston2@gmail.com)"}
+num_records_fetched = 0
+with open(extraction_path, "a") as f:
     for object_id in ids_list:
         if object_id in fetched_ids:
             continue
-        response = requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{object_id}")
+        response = requests.get(f"https://collectionapi.metmuseum.org/public/collection/v1/objects/{object_id}", headers=headers)
         if response.status_code == 200:
             record = response.json()
             f.write(json.dumps(record) + "\n")
-        time.sleep(0.1)
-
+            num_records_fetched +=1
+        else:
+            print(f"skipped {object_id}: status {response.status_code}")        
+        if num_records_fetched % 100 == 0:
+            print(f"{num_records_fetched} records fetched successfully")
+        time.sleep(0.5)
